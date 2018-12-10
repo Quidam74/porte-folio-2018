b41785efa9180dd6153a7a4fa9d282abb8231531
Unknown <toutlemonde74@gmail.com>
2018-12-10 09:20:56 +0100

    window

--- src/js/components/Scroll.js
+++ src/js/components/Scroll.js
@@ -2,14 +2,13 @@
 export default class Scroll {
 
     constructor() {
-        this.$body = document.querySelector(".body");
 
         this.bindEvent();
         this.init();
     }
 
     bindEvent(){
-        this.$body.addEventListener("scroll",(data)=>{
+        window.addEventListener("scroll",(data)=>{
             console.log(data)
         })
     }
